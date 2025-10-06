8939ae522886f9917b722669f8283cee2ab5c946
fuadinadhif <nadhiffuadi@gmail.com>
2025-10-06 20:52:20 +0700

    fix api/index.ts

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,2 +1,7 @@
-import app from "../src/app.js";
-export default app;
+import App from "../src/app.js";
+
+// Create an instance of the app
+const appInstance = new App();
+
+// Export the Express app for Vercel's serverless function handler
+export default appInstance.app;
