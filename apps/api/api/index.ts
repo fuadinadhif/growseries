06cb0fefa63299fd2f220ccd5fa87d3ce1c73804
fuadinadhif <nadhiffuadi@gmail.com>
2025-10-06 21:19:52 +0700

    now??

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,7 +1,5 @@
 import App from "../src/app.js";
 
-// Create an instance of the app
 const appInstance = new App();
 
-// Export the Express app for Vercel's serverless function handler
 export default appInstance.app;
